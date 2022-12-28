007af7a4e5c03ec79e584c073787c70fda8c8790
DaFuqs <dominik.m.schmidt@googlemail.com>
2022-12-28 12:22:09 +0100

    updated to 1.19.3

--- src/main/java/de/dafuqs/pridefurnaces/PrideFurnaces.java
+++ src/main/java/de/dafuqs/pridefurnaces/PrideFurnaces.java
@@ -3,6 +3,7 @@ package de.dafuqs.pridefurnaces;
 import com.mojang.logging.LogUtils;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.Block;
@@ -12,9 +13,10 @@ import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 
 import java.util.ArrayList;
@@ -26,21 +28,20 @@ public class PrideFurnaces implements ModInitializer {
 	protected static final Logger LOGGER = LogUtils.getLogger();
     
     public static final List<String> STUFF_TO_BE_PROUD_OF = List.of("ace", "agender", "aro", "bi", "gay", "genderqueer", "lesbian", "nonbinary", "pan", "trans");
-    public static final FabricItemSettings FURNACE_ITEM_SETTINGS = new FabricItemSettings().group(ItemGroup.DECORATIONS);
     
-    public static List<Block> prideFurnaceBlocks = new ArrayList<>();
+    public static final List<Block> prideFurnaceBlocks = new ArrayList<>();
     public static BlockEntityType<? extends AbstractFurnaceBlockEntity> PRIDE_FURNACE_BLOCK_ENTITY_TYPE;
     
     private static void registerBlock(String name, Block block) {
-        Registry.register(Registry.BLOCK, new Identifier(MOD_ID, name), block);
+        Registry.register(Registries.BLOCK, new Identifier(MOD_ID, name), block);
     }
 
     private static void registerItem(String name, Item item) {
-        Registry.register(Registry.ITEM, new Identifier(MOD_ID, name), item);
+        Registry.register(Registries.ITEM, new Identifier(MOD_ID, name), item);
     }
     
     private static <T extends BlockEntity> BlockEntityType<T> registerBlockEntity(String id, FabricBlockEntityTypeBuilder.Factory<T> factory, Block... blocks) {
-        return Registry.register(Registry.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, id), FabricBlockEntityTypeBuilder.create(factory, blocks).build());
+        return Registry.register(Registries.BLOCK_ENTITY_TYPE, new Identifier(MOD_ID, id), FabricBlockEntityTypeBuilder.create(factory, blocks).build());
     }
 
     @Override
@@ -51,12 +52,18 @@ public class PrideFurnaces implements ModInitializer {
             String blockID = pride + "_furnace";
             Block block = new PrideFurnaceBlock(FabricBlockSettings.copyOf(Blocks.FURNACE));
             registerBlock(blockID, block);
-            registerItem(blockID, new BlockItem(block, FURNACE_ITEM_SETTINGS));
+            registerItem(blockID, new BlockItem(block, new FabricItemSettings()));
             prideFurnaceBlocks.add(block);
         }
         
         Block[] blocks = prideFurnaceBlocks.toArray(new Block[0]);
         PRIDE_FURNACE_BLOCK_ENTITY_TYPE = registerBlockEntity("pride_furnace", PrideFurnaceBlockEntity::new, blocks);
+    
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FUNCTIONAL).register(entries -> {
+            for(Block block : prideFurnaceBlocks) {
+                entries.add(block);
+            }
+        });
         
         log("Startup finished!");
     }
